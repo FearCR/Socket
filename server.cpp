3f4aa21da55d68da1252f696617b8b36f742f8b6
FearCR <39870104+FearCR@users.noreply.github.com>
2019-11-30 17:26:24 -0600

    Add files via upload

--- server.cpp
+++ server.cpp
@@ -3,45 +3,45 @@
 int main(){
   string ip = "localhost";
   string port = "9876";
-  Socket *masterSocket = new Socket(AF_INET,SOCK_STREAM,0); //AF_INET (Internet mode) SOCK_STREAM (TCP mode) 0 (Protocol any)
+  Socket *socket1 = new Socket(AF_INET,SOCK_STREAM,0);
   int optVal = 1;
-  masterSocket->socket_set_opt(SOL_SOCKET, SO_REUSEADDR, &optVal); //You can reuse the address and the port
-  masterSocket->bind(ip, port); //Bind socket on localhost:1234
-  masterSocket->listen(10); //Start listening for incoming connections (10 => maximum of 10 Connections in Queue)
+  socket1->socket_set_opt(SOL_SOCKET, SO_REUSEADDR, &optVal);
+  socket1->bind(ip, port);
+  socket1->listen(10);
 
   while (true) {
-    vector<Socket> reads(1);
-    reads[0] = *masterSocket;
-    int seconds = 10; //Wait 10 seconds for incoming Connections
-    if(Socket::select(&reads, NULL, NULL, seconds) < 1){ //Socket::select waits until masterSocket reveives some input (for example a new connection)
-      //No new Connection
+    vector<Socket> lee(1);
+    lee[0] = *socket1;
+    int seconds = 10;
+    if(Socket::select(&lee, NULL, NULL, seconds) < 1){
+      //No hay coneccion
       continue;
     }else{
-      //Something happens, let's accept the connection
+      //algo se recivio
       break;
     }
   }
-  Socket *newSocket = masterSocket->accept(); //Accept the incoming connection and creates a new Socket to the client
+  Socket *socket2 = socket1->accept();
 
   while (true) {
-    vector<Socket> reads(1);
-    reads[0] = *newSocket;
-    int seconds = 10; //Wait 10 seconds for input
-    if(Socket::select(&reads, NULL, NULL, seconds) < 1){ //Socket::select waits until masterSocket reveives some input (for example a message)
-      //No Input
+    vector<Socket> lee(1);
+    lee[0] = *socket2;
+    int seconds = 10;
+    if(Socket::select(&lee, NULL, NULL, seconds) < 1){
+      //No hay Input
       continue;
     }else{
       string buffer;
-      newSocket->socket_read(buffer, 1024); //Read 1024 bytes of the stream
+      socket2->socket_read(buffer, 1024);
 
-      newSocket->socket_write(buffer); //Sends the input back to the client (echo)
+      socket2->socket_write(buffer);
     }
   }
-  newSocket->socket_shutdown(2);
-  newSocket->close();
+  socket2->socket_shutdown(2);
+  socket2->close();
 
-  masterSocket->socket_shutdown(2);
-  masterSocket->close();
+  socket1->socket_shutdown(2);
+  socket1->close();
 
 
   return  0;
